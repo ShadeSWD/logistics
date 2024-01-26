270f8b752c663d96e86ee960ebed35185e19de8f
ShadeSWD <seawolfy60@gmail.com>
2024-01-26 17:27:41 +0300

    admin action flush debt

--- vendors/admin.py
+++ vendors/admin.py
@@ -2,11 +2,17 @@ from django.contrib import admin
 from vendors.models import Dealer, Product
 
 
+@admin.action(description="Flush debt")
+def flush_debt(modeladmin, request, queryset):
+    queryset.update(debt=0)
+
+
 @admin.register(Dealer)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name', 'dealer_type', 'level', 'contractor', 'debt', 'country', 'city')
     list_filter = ('city', 'country')
     search_fields = ('name',)
+    actions = [flush_debt]
 
 
 @admin.register(Product)
