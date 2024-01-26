5d86e5d6af32972fba335071d66464bda7208912
ShadeSWD <seawolfy60@gmail.com>
2024-01-26 16:51:16 +0300

    filters

--- vendors/views.py
+++ vendors/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter, OrderingFilter
+
 from vendors.models import Dealer, Product
 from vendors.serializers import DealerSerializer, ProductSerializer
 from vendors.paginators import VendorPagination
@@ -22,6 +24,9 @@ class ProductsViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
     pagination_class = VendorPagination
     permission_classes = [IsVendor, ]
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['country']
+    ordering_fields = ['country', 'city']
     queryset = Product.objects.all()
 
     def perform_create(self, serializer):
